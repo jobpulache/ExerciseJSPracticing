fc676e7b142e8bd0d143b2bf3397aff5867175d7
Job <pulachecarrenojob837@gmail.com>
2024-12-02 14:56:42 -0500

    FinishLevelSecond

---
+++ js/questions/levelSecond.js
@@ -0,0 +1,26 @@
+//Para terminar las instrucciones en javascript se utiliza?
+//RPT: O un ";" o un cambio de linea
+
+//Para agrupar varias instrucciones de manera que se ejecuten como un bloque se encierran entre dos símbolos, estos símbolos son:?
+//Llaves
+
+//Para hacer que una o varias instrucciones se ejecuten repetidamente mientras que se cumpla una cierta condición se utiliza una sentencia:?
+//Bucle While
+
+//Si queremos que se ejecute una u otra instrucción en función del resultado de una comparación se utiliza la sentencia¡?
+//If and Else   
+
+//Una instrucción o sentencia de control es una instrucción que?
+//Hace que los programas no se ejecuten de forma secuencial
+
+//Si nuestro programa está ejecutando un bucle es posible hacer que se salte parte de las instrucciones que forman el cuerpo del bucle y vuelva al principio del mismo, para ello usamos la instrucción de control:?
+//Continue
+
+//Cuando la ejecución de una o más instrucciones dependen del resultado de una comparación que puede dar mas de dos resultados usamos la sentencia de control?
+//Switch - case - default
+
+//  Los bucles son un conjunto de instrucciones que se ejecutan de forma repetida y se detienen cuando:?
+//Se deje de cumplir una cierta condicion o aparezca una instrucción break
+
+//Cuando queremos que el cuerpo de un bucle se ejecute como mínimo una vez utilizamos la sentencia:?
+//do . while
