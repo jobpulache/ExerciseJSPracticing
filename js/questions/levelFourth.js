bc76c622daef6f6de76af3716ad83a001ba29173
Job <pulachecarrenojob837@gmail.com>
2024-12-02 16:55:10 -0500

    ending levelFouth

---
+++ js/questions/levelFourth.js
@@ -0,0 +1,31 @@
+//Los evenos del navegador o del documento web son
+//RPTA: Aviso lanzado por el navegador cuando ocurre algun cambio
+
+//Si oyes hablar del DOM en relación a Javascript ¿están hablando de?
+//RPTA: Objetos que ve JS en la página web
+
+//Para construir páginas web interactivas ...
+//RPTA:JS and HTML Is necesary
+
+//Para que la página web reaccione cuano el usuario pulsa una tecla...
+//RPTA: Use events JS
+
+//El objeto window tal y como lo percibe javascript se denomina
+//RPT: Model DOM
+
+//Las páginas web interactivas son páginas web que...
+//ROPTA:They respond at actions the user
+
+//Cuando hablamos de páginas escritas en DHTML nos referimos a páginas escritas usando?
+//HTML, CSS AND JS
+
+//Los guiones o scripts se incluyen en las páginas web?
+//Cargando desde archivos externos o en la pàgina
+
+//La palabra DOM se refiere a?
+//RPTA: It's a modek data creating for the browser's for enlace JS - Modelo de objetos creado por los navegadores para enlazar con Javascript
+
+
+//En el entrono de desarrollo web ¿qué es el BOM?
+//RPTA: Un modelo de objetos que expone el navegador a Javascript. - 
+
