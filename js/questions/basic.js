e339aa4269492011e32ef151d61778a1ff062be7
Job <pulachecarrenojob837@gmail.com>
2024-12-02 14:29:56 -0500

    questionsLevelBasic

---
+++ js/questions/basic.js
@@ -0,0 +1,19 @@
+//Questions:
+//1.- Para escribir un programa usando Javascript se necesita?
+// RPTA= One text editor simple
+
+//2.-Los lenguajes Javascript y Java son lenguajes de programación que tienen en común?
+// RPT=>Ambos usan objetos
+
+//3.-Cuando una página utiliza funciones escritas en Javascript, estas funciones se ejecutan?
+//RPT=>  Pueden ejecutarse cuando son llamadas desde algún otro script o a causa de un evento.
+
+//4.-Los programas escritos en Javascript que etán presetnes dentro de una página se ejecutan?
+//RPT=>En la página del cliente o visitante del sitio Web
+
+//5.-Los llamados scripts o guiones que se usan en las páginas web son?
+//RPTA=>Programas que son utilizados por el navegador
+
+//6.-Cuando colocamos una función escrita en el lenguaje javascript dentro de la página web, El código de la función se coloca?
+//RPTA=>Puede colocarse en la sección HEAD o sectión BODY
+
