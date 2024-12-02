5a82a8720338fa20b992dd7bc189489272e71c34
Job <pulachecarrenojob837@gmail.com>
2024-12-02 14:37:34 -0500

    addLevelOne

---
+++ js/questions/levelOne.js
@@ -0,0 +1,6 @@
+//Los lenguajes de JS And Java son lenguajes de programación que tienen en comun:?
+//RPT: Ambos usan objetos
+
+//Los llamados scripts o guiones que se usan en las páginas web son?
+//RPT: Programas que son ejecutados por el navegador
+
