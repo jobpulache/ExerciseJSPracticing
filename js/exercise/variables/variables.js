f4b2e84b7012225211403a975f893b859438d9bd
Job <pulachecarrenojob837@gmail.com>
2024-12-03 14:56:28 -0500

    ending exercise varible

---
+++ js/exercise/variables/variables.js
@@ -0,0 +1,11 @@
+/*Has hecho una compra y sabes el precio del producto y su iva. Haz un script que te calcule el precio total que vas a pagar por tu compra.
+Te recuerdo que para calcular el total debes sumar al precio el resultado de multiplicasr precio por el iva y dividir por 100*/
+
+let precio = 200
+let iva = 21
+let total = precio + precio * iva / 100
+
+
+console.log(total);
+
+
