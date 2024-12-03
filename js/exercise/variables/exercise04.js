a7454e6fff8b6f750b77362191d940eb8e75cf8f
Job <pulachecarrenojob837@gmail.com>
2024-12-03 16:48:06 -0500

    ending exercise04

---
+++ js/exercise/variables/exercise04.js
@@ -0,0 +1,8 @@
+/*En este ejercicio vas a pedir al usuario que teclee tres números enteros y el script mostrará como resultado el valor medio de los tres.
+Recuerda que la media de tres números se calcula sumando los tres y dividiendo entre 3.*/
+
+let n1 = parseInt(prompt("Enter the first number"))
+let n2 = parseInt(prompt("Enter the second number"))
+let n3 = parseInt(prompt("Enter the third number"))
+let media = (n1+n2+n3)/3
+console.log(media);
