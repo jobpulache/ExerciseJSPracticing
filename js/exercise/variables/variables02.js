ea82d0b6ab11e0bc95259e2456cdf9eea4da0752
Job <pulachecarrenojob837@gmail.com>
2024-12-03 15:06:51 -0500

    ending exercise variable02

---
+++ js/exercise/variables/variables02.js
@@ -0,0 +1,8 @@
+/*En una variable tienes el lado de un cuadrado, debes escribir un programa que te calcule el área y el perímetro del cuadrado.
+El area la calculas como lado multiplicado por lado. El perímetro es la suma de los cuatro lados.*/
+let ladoCuadrado = 3;
+let area= ladoCuadrado * ladoCuadrado
+let perimetro= ladoCuadrado + ladoCuadrado + ladoCuadrado + ladoCuadrado
+
+console.log(`el área del cuadrado es: ${area} y su perimetro es: ${perimetro}`);
+
