a6badd39b49b479e99a972ca9651d945289f8eab
Job <pulachecarrenojob837@gmail.com>
2024-12-11 18:11:01 -0500

    ending first exercise conditionals

---
+++ js/exercise/conditionalOne/exercise01.js
@@ -0,0 +1,8 @@
+/*Se trata de escribir un script que diga si un número es par o es impar.
+Recordemos que un número es par si al dividirlo por 2 da como resto 0.*/
+
+let num = prompt("Enter num int")
+if ( num % 2 ===0){
+    alert("Is number whole")
+}
+alert("not Whole")
